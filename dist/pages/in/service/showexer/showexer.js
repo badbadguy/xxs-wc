0918bca3aee6755a251a07421ce379f537aa590e
badbadguy <yuan610136@163.com>
2019-03-25 18:38:05 +0800

    1

--- dist/pages/in/service/showexer/showexer.js
+++ dist/pages/in/service/showexer/showexer.js
@@ -21,35 +21,26 @@ exports.default = Page({
       }, {
         title: '舱内保险丝盒无火烧或熏黑痕迹'
       }]
-    }, {
-      title: '排除水泡车',
-      number: 8,
-      state: 'normal',
-      stateNum: 5,
-      content: [{
-        title: '防火墙无火烧或熏黑痕迹'
-      }, {
-        title: '发动机线束无火烧或熏黑痕迹'
-      }, {
-        title: '车辆覆盖件无火烧或熏黑痕迹'
-      }, {
-        title: '舱内保险丝盒无火烧或熏黑痕迹'
-      }]
-    }, {
-      title: '排除水泡车',
-      number: 8,
-      state: 'normal',
-      stateNum: 5,
-      content: [{
-        title: '防火墙无火烧或熏黑痕迹'
-      }, {
-        title: '发动机线束无火烧或熏黑痕迹'
-      }, {
-        title: '车辆覆盖件无火烧或熏黑痕迹'
-      }, {
-        title: '舱内保险丝盒无火烧或熏黑痕迹'
-      }]
     }]
+  },
+  onLoad: function onLoad() {
+    var that = this;
+    wx.request({
+      url: getApp().globalData.headurl + 'question/select1',
+      header: {
+        'content-type': 'application/json'
+      },
+      data: {
+        question_type: "1",
+        subject_id: "33018ef1b3b74a18b6d9f94bff995d79"
+      },
+      success: function success(res) {
+        console.log(res);
+        that.setData({
+          accordion: res.data.resultList
+        });
+      }
+    });
   },
   navigateBack: function navigateBack() {
     wx.navigateBack();
