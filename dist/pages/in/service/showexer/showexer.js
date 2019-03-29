96769ae2e79b6ca38ffd046b5aacdbff143384df
lhb <yuan610136@163.com>
2019-03-30 01:14:21 +0800

    1

--- dist/pages/in/service/showexer/showexer.js
+++ dist/pages/in/service/showexer/showexer.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = Page({
   data: {
     current1: 0,
-    subject_id: 'cd84a79d6ee04e4d9630731b25b589d0',
+    subject_id: '33018ef1b3b74a18b6d9f94bff995d79',
     items: [],
     accordion: [{
       title: '查询出错',
@@ -15,8 +15,21 @@ exports.default = Page({
       stateNum: 5,
       content: []
     }],
-    heightAuto: "100%"
+    heightAuto: "200%"
   },
+  slide: function slide(res) {
+    var that = this;
+    if (that.data.current1 == '2' && that.data.subject_id == 'cd84a79d6ee04e4d9630731b25b589d0') {
+      var tempheight = (that.data.accordion[parseInt(res.currentTarget.id)].children.length + 1) * 100;
+    } else {
+      var tempheight = 100;
+    }
+    console.log(tempheight);
+    that.setData({
+      heightAuto: tempheight + '%'
+    });
+  },
+
   onLoad: function onLoad() {
     this.question(0);
   },
@@ -32,8 +45,10 @@ exports.default = Page({
         subject_id: that.data.subject_id
       },
       success: function success(res) {
+        var tempheight = res.data.resultList.length * 50;
         that.setData({
-          accordion: res.data.resultList
+          accordion: res.data.resultList,
+          heightAuto: tempheight + 'px'
         });
       }
     });
